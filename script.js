1f7df6016b81fa7fc803ee721810966f17fbd6a1
Zuzanna Skokowska <zuzanna.skokowska@icloud.com>
2023-06-06 09:35:27 +0200

    add js to bookmark and button hide and show

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-const mainElement = document.querySelector("main");
-const showButtons = document.querySelectorAll(".show");
-const hideButtons = document.querySelectorAll(".hide");
+const showButton = document.querySelector('[data-js="show-button"]');
 const bookmarkButton = document.querySelector(".mark");
 const answer = document.querySelector("[data-js='answer-par']");
 const card = document.querySelector(".question-card");
 
-showButtons.forEach(function (showButton) {
-  showButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("klik");
-    answer.classList.add("answer-showed");
-    showButton.remove();
-    // now create element hide button;
-    // try to do this for all paragraphs;
-  });
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmarked");
 });
 
-hideButtons.forEach(function (hideButton) {
-  hideButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("klik");
-    answer.classList.add("answer");
-    hideButton.classList.remove("hide");
-    hideButton.classList.add("show");
-    hideButton.innerHTML = "Show Answer";
-  });
+showButton.addEventListener("click", () => {
+  answer.classList.toggle("answer-showed");
+  showButton.classList.toggle("hide");
 });
+buttonTextToggle();
+
+function buttonTextToggle() {
+  // console.log("click");
+  let button = document.getElementById("1button");
+  if (button.innerHTML == "Show answer") {
+    button.innerHTML = "Hide answer";
+  } else {
+    button.innerHTML == "Hide answer";
+    button.innerHTML = "Show answer";
+  }
+}
